af47d6dae3c8f49fd0a092413134acba190d699d
KarenAguayo <karen.aguayo@ucb.edu.bo>
2023-03-23 08:55:46 -0400

    2 ciclo tdd retorna el mismo numero para la cadena de uno

--- src/sumador.js
+++ src/sumador.js
@@ -1,5 +1,7 @@
-function calculadora() {
-  return 0;
+function calculadora(texto) {
+  if(texto=="")
+   return 0;
+  return(parseInt(texto));
 }
 
 export default calculadora;
--- src/sumador.test.js
+++ src/sumador.test.js
@@ -2,6 +2,9 @@ import calculadora from "./sumador.js";
 
 describe("calculadora", () => {
   it("deberia retornar 0 para una cadena vacia", () => {
-    expect(calculadora()).toEqual(0);
+    expect(calculadora("")).toEqual(0);
+  });
+  it("deberia retornar el mismo numero para una cadena de un numero", () => {
+    expect(calculadora("1")).toEqual(1);
   });
 });
