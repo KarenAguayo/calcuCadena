21da7c83aad17a57f2899617f2d43efeeb02fb2b
KarenAguayo <karen.aguayo@ucb.edu.bo>
2023-03-23 09:35:06 -0400

    3 ciclo tdd suma una cadena separada por comas y los suma

--- src/sumador.js
+++ src/sumador.js
@@ -1,7 +1,17 @@
 function calculadora(texto) {
   if(texto=="")
    return 0;
-  return(parseInt(texto));
+  let numeros = texto.split(",");
+  if(numeros.length==1){
+    return parseInt(texto);
+  }
+  else{
+   let num1 = parseInt(numeros[0]);
+   let num2 = parseInt(numeros[1]);
+   var sumando = num1 + num2;
+  }
+  
+  return sumando;
 }
 
 export default calculadora;
--- src/sumador.test.js
+++ src/sumador.test.js
@@ -7,4 +7,7 @@ describe("calculadora", () => {
   it("deberia retornar el mismo numero para una cadena de un numero", () => {
     expect(calculadora("1")).toEqual(1);
   });
+  it("deberia retornar la suma delimitada por , de una cadena", () => {
+    expect(calculadora("1,2")).toEqual(3);
+  });
 });
