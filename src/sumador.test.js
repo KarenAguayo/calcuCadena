60b7a8972ea241a803faf0bf1abd309700c4dd4e
KarenAguayo <karen.aguayo@ucb.edu.bo>
2023-03-27 23:07:41 -0400

    la prueba esta pasando

--- src/sumador.test.js
+++ src/sumador.test.js
@@ -22,4 +22,7 @@ describe("calculadora", () => {
   it("deberia retornar la suma delimitada por - de dos numeros", () => {
     expect(calculadora("6-4-8")).toEqual(18);
   });
+  it("deberia retornar la suma delimitada por -, de cadena de numeros", () => {
+    expect(calculadora("2,8-10")).toEqual(20);
+  });
 });
