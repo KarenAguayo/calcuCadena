ece6e7ab426289e99416276b6257a0917ca7551f
KarenAguayo <karen.aguayo@ucb.edu.bo>
2023-03-30 09:43:03 -0400

    refactoring se elimino codigo duplicado y redundante

--- src/sumador.js
+++ src/sumador.js
@@ -5,27 +5,17 @@ function calculadoraDeCadenas(cadena) {
     return resultado = 0; 
   }
   if(cadena.search(",") != -1 || cadena.search("-") != -1) {
-    const cadenaArray = cadena.split(/[,-]+/);
     return resultado = splitAdd(cadena, "");
   } if(cadena[0]=== "/"){
-    return resultado = get_separator(cadena);
+    return resultado;
   }else return resultado = parseInt(cadena);
 }
-
-function get_separator(cadena){
-  var cadenaArray;
-  cadenaArray = cadena.split(/[[\n ]+/);
-  return splitAdd(cadenaArray[2], cadenaArray[1].slice(0,-1));
-}
-
 function splitAdd(cadena, separador){
   var cadenaArray;
   if(separador === ""){
     cadenaArray = cadena.split(/[,-]+/);
   }else cadenaArray = cadena.replaceAll(separador, ',').split(/[,-]+/)
-  {
     return sumarCadena(cadenaArray);
-  }
 }
 function sumarCadena (cadenaArray){
   var resultado=0;
